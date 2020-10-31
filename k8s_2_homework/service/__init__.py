ae9f5f847f12e57bda3c0b563d28aab6bb48b2f3
Vadim Zaitsev <v.zaitsev@corp.101xp.com>
2020-10-31 15:31:06 +0300

    code upd. ingress added

--- k8s_2_homework/service/__init__.py
+++ k8s_2_homework/service/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+import time
 
 app = Flask(__name__)
+
+time.sleep(10)
 
 from service import routes
--- k8s_2_homework/service/routes.py
+++ k8s_2_homework/service/routes.py
@@ -3,10 +3,16 @@ from service import app
 from flask import jsonify
 import os
 
+host=os.environ.get('HOSTNAME', 'unknown')
+
 @app.route('/')
 def index():
-    return 'Превед медвед!\r\nНа связи: ' + os.environ['HOSTNAME'] + '\r\n'
+    return 'Превед медвед!\r\nНа связи: ' + host
 
-@app.route('/health/')
+@app.route('/health')
 def health():
     return jsonify(status='OK')
+
+@app.route('/version')
+def version():
+    return jsonify(version=3, host=host)
