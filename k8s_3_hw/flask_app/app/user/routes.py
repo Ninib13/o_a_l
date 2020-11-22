48e148c53658dfa0da8301a745b0f09a477d9b71
Vadim Zaitsev <v.zaitsev@corp.101xp.com>
2020-11-22 18:41:47 +0300

    update user funct added

--- k8s_3_hw/flask_app/app/user/routes.py
+++ k8s_3_hw/flask_app/app/user/routes.py
@@ -47,3 +47,43 @@ def getUser(user_id):
         }), 404
     
     return jsonify(user_schema.dump(u))
+
+
+@bp.route('/<user_id>', methods=['DELETE'])
+def deleteUser(user_id):
+    logger.info('delete user {}'.format(user_id))
+    u = User.query.get(user_id)
+    if not u:
+        return jsonify({
+            "code": 404,
+            "message": "user not found"
+        }), 404
+    
+    db.session.delete(u)
+    db.session.commit()
+
+    return "OK", 204
+
+@bp.route('/<user_id>', methods=['PUT'])
+def updateUser(user_id):
+    logger.info('update user {}'.format(user_id))
+    u = User.query.get(user_id)
+    if not u:
+        return jsonify({
+            "code": 404,
+            "message": "user not found"
+        }), 404
+
+    json_data = request.get_json()
+    if not json_data:
+        return {"message": "No input data provided"}, 400
+    # Validate and deserialize input
+    try:
+        data = user_schema.load(json_data)
+    except ValidationError as err:
+        return err.messages, 422
+    
+    db.session.query(User).filter(User.id == user_id).update(data)
+    db.session.commit()
+
+    return "OK", 200
